dfd2a8c96e6742223ab833e1bd43d1ff36fa7829
mariana-calado <marianacalado01@gmail.com>
2026-04-15 13:38:56 -0300

    databse.sql corrigido

--- tem-ai-backend/database.sql
+++ tem-ai-backend/database.sql
@@ -12,44 +12,43 @@ CREATE TABLE Usuario (
     cep VARCHAR(8),
     logradouro VARCHAR(150),
     cidade VARCHAR(50),
-    estado CHAR(2),
-    apartamento VARCHAR(10),
-    tipo VARCHAR(20)
+    estado CHAR(2)
 );
 
 CREATE TABLE Produto (
     ID_produto INT PRIMARY KEY AUTO_INCREMENT,
-    nome Varchar(100),
-    preco Decimal(10,2),
+    nome VARCHAR(100),
+    preco DECIMAL(10,2),
     estoque_atual INT
 );
 
 CREATE TABLE Pedido (
     ID_pedido INT PRIMARY KEY AUTO_INCREMENT,
-    data_hora DateTime,
-    status Varchar(30),
-    valor_total Decimal(10,2),
+    data_hora DATETIME,
+    status VARCHAR(30),
+    valor_total DECIMAL(10,2),
     fk_Usuario_ID_usuario INT
 );
 
 CREATE TABLE pedido_produto (
     fk_Pedido_ID_pedido INT,
-    fk_Produto_ID_produto INT NULL,
-    quantidade INT
+    fk_Produto_ID_produto INT,
+    quantidade INT,
+	PRIMARY KEY (fk_Pedido_ID_pedido, fk_Produto_ID_produto)
 );
- 
+
 ALTER TABLE Pedido ADD CONSTRAINT FK_Pedido_2
     FOREIGN KEY (fk_Usuario_ID_usuario)
     REFERENCES Usuario (ID_usuario)
     ON DELETE CASCADE;
- 
+
 ALTER TABLE pedido_produto ADD CONSTRAINT FK_pedido_produto_1
     FOREIGN KEY (fk_Pedido_ID_pedido)
     REFERENCES Pedido (ID_pedido)
     ON DELETE RESTRICT;
- 
+
 ALTER TABLE pedido_produto ADD CONSTRAINT FK_pedido_produto_2
     FOREIGN KEY (fk_Produto_ID_produto)
     REFERENCES Produto (ID_produto)
-    ON DELETE SET NULL;
+    ON DELETE CASCADE;
 
