e8fab6095f42d83307e05cfe67e38462c11dd30d
Lucas <lcsgirata@gmail.com>
2026-05-07 19:23:39 -0300

    add user section on header

--- tem-ai-backend/app.py
+++ tem-ai-backend/app.py
@@ -23,7 +23,7 @@ def conectar():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="12345678",
+        password="lucaS1712",
         database="tem_ai"
     )
 
